b2c69e73070ca9857d53acd00bbc6968045ee947
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2025-12-08 17:06:20 -0500

    Added bite and double shot

---
+++ Assets/Scripts/Enemy/States/Attack/DoubleShotAttackState.cs
@@ -0,0 +1,62 @@
+﻿using LBG;
+using UnityEngine;
+
+namespace EnemyStates
+{
+    [System.Serializable]
+    [SubclassPath("Gun", "Double Shot")]
+    public class DoubleShotAttackState : AttackState
+    {
+        [SerializeField] private BulletSpawner _rightBulletSpawner;
+        [SerializeField] private BulletSpawner _leftBulletSpawner;
+        [SerializeField] private float _bulletSpeed = 10f;
+        [SerializeField] private float _bulletLifespan = 5f;
+        [SerializeField] private float _bulletDamage = 1f;
+        [SerializeField] private int _shootCount = 1;
+        [SerializeField] private float _shootCooldown = 1f;
+        
+        private int _currentShootCount;
+        private float _currentShootCooldownTimer;
+        
+        private protected override void OnEnter()
+        {
+            _currentShootCount = 0;
+            _currentShootCooldownTimer = _shootCooldown; // shoot immediately
+        }
+
+        private protected override void OnExit()
+        {
+            
+        }
+
+        private protected override void OnUpdate()
+        {
+            HandleShootingCooldown();
+        }
+
+        private protected override void OnFixedUpdate()
+        {
+            
+        }
+
+        private protected override State<Enemy> GetTransition()
+        {
+            if (_currentShootCount >= _shootCount && _currentShootCooldownTimer >= _shootCooldown)
+                return _context.PatrolState;
+            
+            return null;
+        }
+
+        private void HandleShootingCooldown()
+        {
+            _currentShootCooldownTimer += Time.deltaTime;
+            if (_currentShootCooldownTimer >= _shootCooldown && _currentShootCount < _shootCount)
+            {
+                _rightBulletSpawner.Spawn(_context.Team, _context.ForwardDirection, _bulletSpeed, _bulletLifespan, _bulletDamage);
+                _leftBulletSpawner.Spawn(_context.Team, _context.ForwardDirection, _bulletSpeed, _bulletLifespan, _bulletDamage);
+                _currentShootCount++;
+                _currentShootCooldownTimer = 0f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/States/Attack/MeleeBiteAttackState.cs
@@ -0,0 +1,63 @@
+﻿using LBG;
+using UnityEngine;
+
+namespace EnemyStates
+{
+    [System.Serializable]
+    [SubclassPath("Melee", "Bite")]
+    public class MeleeBiteAttackState : AttackState
+    {
+        [SerializeField] private Vector2 _biteOffset = new Vector2(0.5f, 0f);
+        [SerializeField] private float _biteRadius = 0.5f;
+        [SerializeField] private float _damage = 1f;
+        [SerializeField] private float _cooldown = 1f;
+
+        private float _timer;
+        
+        private protected override void OnEnter()
+        {
+            _timer = 0f;
+            
+            Collider2D hit = Physics2D.OverlapCircle((Vector2)_context.transform.position + _context.ForwardDirection * _biteOffset, _biteRadius,
+                LayerMask.GetMask("Hitbox"));
+
+            if (hit == null)
+                return;
+            
+            Health health = hit.GetComponentInParent<Health>();
+            if (health == null)
+                return;
+            ITeam team = hit.GetComponentInParent<ITeam>();
+            if (team == null)
+                return;
+
+            if (team.Team == _context.Team)
+                return;
+            
+            health.TakeDamage(_damage);
+        }
+
+        private protected override void OnExit()
+        {
+            
+        }
+
+        private protected override void OnUpdate()
+        {
+            _timer += Time.deltaTime;
+        }
+
+        private protected override void OnFixedUpdate()
+        {
+            
+        }
+
+        private protected override State<Enemy> GetTransition()
+        {
+            if (_timer >= _cooldown)
+                return _context.IdleState;
+            
+            return null;
+        }
+    }
+}
